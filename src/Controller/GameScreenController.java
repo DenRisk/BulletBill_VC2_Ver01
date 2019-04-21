9555e9aaee00dcace1cf0cc6fee0c6a9d522a033
DenRisk <DenRisk@users.noreply.github.com>
2019-04-21 12:25:35 +0200

    GUI Fertig

--- src/Controller/GameScreenController.java
+++ src/Controller/GameScreenController.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class GameScreenController {
+
+    @FXML
+    private Button closebtn;
+    @FXML
+    private Button playbtn;
+    @FXML
+    private Button breakbtn;
+    @FXML
+    private Button repeatbtn;
+    @FXML
+    private Label fpsLabel;
+
+    public void quitGame() {
+        Stage stage = (Stage) closebtn.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,51 @@
 package Main;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 public class Main extends Application {
+
+    private static Parent root;
+    private double xOffset = 0;
+    private double yOffset = 0;
 
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartScreen.fxml"));
+        root = FXMLLoader.load(getClass().getResource("/fxml/StartScreen.fxml"));
         stage.setTitle("BulletBill");
         stage.setScene(new Scene(root, 1000, 700));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setResizable(false);
         stage.show();
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
     }
+
+
+
+
 
     public static void main (String[] args) {
         launch(args);
